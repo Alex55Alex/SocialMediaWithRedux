447d8afe7ea5bfdbc8662b4f79e83da14a57e0b1
Aleksey Sukhetskyi <oleksiy.s228s@gmail.com>
2021-05-24 12:33:21 +0300

    feat : add Dialogs styles

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+
 const Dialogs: (props: object) => JSX.Element = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    Vova
+                </div>
+                <div className={s.dialog}>
+                    Petya
+                </div>
+                <div className={s.dialog}>
+                    Evgen
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Vasya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    How are you?)
+                </div>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    I am very tired
+                </div>
+            </div>
         </div>
     )
 }
